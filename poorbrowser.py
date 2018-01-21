d54f27953bbfc84dbf9c2ae85737cbfde727df90
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-01-21 15:14:32 +0100

    The first deployable version

--- poorbrowser.py
+++ poorbrowser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, send_from_directory, safe_join, jsonify, request
-from flaskrun import flaskrun
+from flask.ext.api import status
 import os
 
 try:
@@ -15,26 +15,57 @@ app = Flask(__name__)
 def index():
     return send_from_directory(os.getcwd(), "index.html")
 
-@app.route("/api1/")
-def root():
+@app.route("/v1/")
+def list_files():
     return jsonify(
         {
             "files": sort(f for f in os.listdir(".")
                           if not os.path.isdir(f)),
         })
 
-@app.route("/api1/<filename>", methods=['GET', 'DELETE'])
+@app.route("/v1/<filename>", methods=['GET', 'DELETE', 'PUT'])
 def serve_file(filename):
     def get_file():
         return send_from_directory(os.getcwd(), filename)
+
+    def store_file():
+        created = not os.path.exists(filename)
+        with open(filename, 'wb') as stored_image:
+            stored_image.write(request.data)
+
+        if created:
+            return (
+                jsonify({'result': 'created'}),
+                status.HTTP_201_CREATED
+            )
+        else:
+            return (
+                jsonify({'result': 'ok'}),
+            )
+
     def delete_file():
-        os.unlink(filename)
-        return jsonify({'result': 'ok'})
+        try:
+            os.unlink(filename)
+        except FileNotFoundError:
+            return (
+                jsonify({
+                    'error': {
+                        'not_found': filename,
+                    },
+                }),
+                status.HTTP_404_NOT_FOUND
+            )
+        else:
+            return (
+                jsonify({'result': 'deleted'}),
+            )
 
     return {
         'GET': get_file,
+        'PUT': store_file,
         'DELETE': delete_file,
     }[request.method]()
 
 if __name__ == '__main__':
+    from flaskrun import flaskrun
     flaskrun(app, threaded=True)
